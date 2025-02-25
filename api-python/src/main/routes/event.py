d7813fb3cb3056eb05fcf911a481e7eb90a1756e
flvsantos15 <flvsantos300@gmail.com>
2025-02-24 22:30:04 -0300

    feat: add http methods types

--- api-python/src/main/routes/event.py
+++ api-python/src/main/routes/event.py
@@ -1,7 +1,12 @@
 from flask import jsonify, Blueprint
+from src.http_types.http_response import HttpResponse
+from src.http_types.http_request import HttpRequest
 
 event_route_bp = Blueprint('event_route_bp', __name__)
 
 @event_route_bp.route('/events', methods=['POST'])
 def create_new_event():
-  return jsonify({ "estou aqui" }), 201
+  http_request = HttpRequest(body=request.json)
+
+  http_response = HttpResponse(body={ "estou aqui" }, status_code=201)
+  return jsonify(http_response.body), http_response.status_code
