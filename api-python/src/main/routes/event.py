5cd7eaf33d38f3e6172d6ec2350df0ce447d2e79
flvsantos15 <flvsantos300@gmail.com>
2025-02-25 23:14:39 -0300

    feat: add subscriber event validator

--- api-python/src/main/routes/event.py
+++ api-python/src/main/routes/event.py
@@ -1,11 +1,13 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 from src.http_types.http_response import HttpResponse
 from src.http_types.http_request import HttpRequest
+from src.validators.events_creator_validator import events_creator_validator
 
 event_route_bp = Blueprint('event_route_bp', __name__)
 
 @event_route_bp.route('/events', methods=['POST'])
 def create_new_event():
+  events_creator_validator(request)
   http_request = HttpRequest(body=request.json)
 
   http_response = HttpResponse(body={ "estou aqui" }, status_code=201)
