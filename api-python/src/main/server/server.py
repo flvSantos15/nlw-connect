43d38e945ba12732f5f113b432af131810a3a416
flvsantos15 <flvsantos300@gmail.com>
2025-03-03 17:49:57 -0300

    chore: add events_link_route to server

--- api-python/src/main/server/server.py
+++ api-python/src/main/server/server.py
@@ -1,8 +1,10 @@
 from flask import Flask # type: ignore
 from src.main.routes.event import user_route_bp
 from src.main.routes.subscriber import subscriber_route_bp
+from src.main.routes.events_link import events_link_route_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(user_route_bp)
 app.register_blueprint(subscriber_route_bp)
+app.register_blueprint(events_link_route_bp)
