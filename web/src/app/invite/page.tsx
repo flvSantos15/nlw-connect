be522540bbcd5464b32149c6349961b62c0ace1a
flvsantos15 <flvsantos300@gmail.com>
2025-03-05 11:55:08 -0300

    chore: add medals box

--- web/src/app/invite/page.tsx
+++ web/src/app/invite/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 
-import logo from "@/assets/logo.svg";
-import { InputField, InputIcon, InputRoot } from "@/component/input";
 import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from "lucide-react";
+
+import { InputField, InputIcon, InputRoot } from "@/component/input";
 import { IconButton } from "@/component/icon-button";
+
+import logo from "@/assets/logo.svg";
+import cooper from "@/assets/medal-cooper.svg";
+import gold from "@/assets/medal-gold.svg";
+import silver from "@/assets/medal-silver.svg";
 
 export default function Invite() {
   return (
@@ -80,6 +85,50 @@ export default function Invite() {
 
               <Medal className="size-5 text-purple absolute top-3 left-3" />
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full max-w-[440px] space-y-5">
+        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+          Ranking de indicacoes
+        </h2>
+
+        <div className="space-y-4">
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">1º</span> | Flavio Santos
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+
+            <Image src={gold} alt="" className="absolute top-0 right-8" />
+          </div>
+
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">2º</span> | Flavio Santos
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+
+            <Image src={silver} alt="" className="absolute top-0 right-8" />
+          </div>
+
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">3º</span> | Flavio Santos
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+
+            <Image src={cooper} alt="" className="absolute top-0 right-8" />
           </div>
         </div>
       </div>
